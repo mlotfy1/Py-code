14191dce271deb238a32ec2d36aa56e9cc0513c9
Mohamed Lotfy <mohamed.lotfy@vodafone.com>
2018-02-10 01:53:14 +0200

    loop fuction

---
+++ py08.py
@@ -0,0 +1,16 @@
+from __future__ import print_function
+
+numbers = range(5,50,10)
+
+print(numbers)
+
+for number in numbers:
+    print(number)
+##############################################
+
+names = ['Ben','Alice','Jack','Sarah']
+
+print(names)
+
+for name in names:
+    print(name,'is a great person!')
