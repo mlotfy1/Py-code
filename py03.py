32c690d0244dde12cb4576150787e94705416acc
Mohamed Lotfy <mohamed.lotfy@vodafone.com>
2018-02-10 01:04:04 +0200

    input/output codes

---
+++ py03.py
@@ -0,0 +1,4 @@
+from __future__ import print_function
+print('Hello World!', end='')
+print('Hello','World','!', sep='')
+print('This is', 'an example')
---
+++ py04.py
@@ -0,0 +1,3 @@
+from __future__ import print_function
+userName = raw_input( 'Please enter your name: ' )
+print('Hello', userName, '!')
