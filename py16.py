bfaae8ab3952cf576c8ebbe3d432ffec8b351b47
Mohamed Lotfy <mohamed.lotfy@vodafone.com>
2018-02-12 01:57:30 +0200

    Exp func from functions folder

---
+++ py16.py
@@ -0,0 +1,11 @@
+from __future__ import print_function
+
+import sys
+sys.path.insert(0, 'functions')
+
+import exp_func
+
+base = input('Please enter the base value: ')
+exponent = input('Please enter the exponent value: ')
+
+exp_func.exp(base,exponent)
