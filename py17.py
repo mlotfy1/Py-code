0d4600495c697df879c45b54e563e03849f667c4
Mohamed Lotfy <mohamed.lotfy@vodafone.com>
2018-02-16 00:36:53 +0200

    error handling code 1

---
+++ py17.py
@@ -0,0 +1,21 @@
+from __future__ import print_function
+
+while True:
+    try:
+        print("Let us solve the equation (x/2) / (x-y) ")
+        print("Please enter 0 to exit")
+
+        x = float(input("Please enter a value for x: "))
+        y = float(input("Please enter a value for y: "))
+
+        if x==0 or y==0:
+            break
+
+        z = (x/2) / (x-y)
+
+        print("Solving (x/2) / (x-y) for values x=", \
+              x,"and y=",y,"we get the result:",z)
+
+    except:
+        print("There was an error with the code")
+        
