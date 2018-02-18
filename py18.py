9f1cfc956d9d0718ec3e384d34acb4210289d7ba
Mohamed Lotfy <mohamed.lotfy@vodafone.com>
2018-02-19 01:49:30 +0200

    Error handling example 2

---
+++ py18.py
@@ -0,0 +1,31 @@
+from __future__ import print_function
+
+while True:
+    try:
+        print("Let us solve equation (x/2) / (x-y) ")
+        print("Please enter 0 to exit")
+
+        x = float(input("Please enter a value for x: "))
+        y = float(input("Please enter a value for y: "))
+
+        if x==0 or y==0:
+            break
+
+        z = (x/2) / (x-y)
+
+    except ZeroDivisionError as e:
+        print("There was an error with the code")
+        print("you keyed in a value that caused a division by zero")
+    except NameError as e:
+        print("THere was an error with the code")
+        print("You keyed in a text value where a number was expected")
+    except Exception as e:
+        print("there was an unknown error with the code")
+        print("Error message:",str(e))
+
+    else:
+        print("solving (x/2) / (x-y) for values x=", \
+              x,"and y=",y,"we get the result",z)
+    finally:
+        #cleanup code
+        print("Cleanup code goes here!")
